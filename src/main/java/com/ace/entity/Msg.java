238e337256d6b01a7e4ef6f803eb346555678170
bamboox <892078604@qq.com>
2017-11-28 10:14:38 +0800

    tidy up

--- src/main/java/com/ace/entity/Msg.java
+++ src/main/java/com/ace/entity/Msg.java
@@ -1,11 +1,13 @@
 package com.ace.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
  * @author bamboo
  */
 @Data
+@AllArgsConstructor
 public class Msg {
     private String title;
     private String content;
