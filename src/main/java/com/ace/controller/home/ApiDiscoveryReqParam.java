2f8d52e1478c0cb56fcb12adfbdb21b877cd520e
bamboox <892078604@qq.com>
2017-12-19 21:45:52 +0800

    update

--- src/main/java/com/ace/controller/home/ApiDiscoveryReqParam.java
+++ src/main/java/com/ace/controller/home/ApiDiscoveryReqParam.java
@@ -1,27 +1,23 @@
 package com.ace.controller.home;
 
-import java.util.List;
+import com.ace.common.base.ApiBaseFileReqParam;
+import lombok.Data;
 
 import javax.validation.constraints.NotNull;
-
-import com.ace.common.base.ApiBaseFileReqParam;
-import com.ace.common.base.ApiBaseReqParam;
-import com.ace.entity.Discovery;
-
-import com.alibaba.fastjson.annotation.JSONField;
-
-import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
+import java.util.List;
 
 /**
  * Created by bamboo on 17-12-2.
  */
 @Data
 public class ApiDiscoveryReqParam {
-
+    @NotNull
     private double latitude;
+    @NotNull
     private double longitude;
+    @NotNull
     private String location;
+    @NotNull
     private String description;
     @NotNull
     private String sendDepartmentId;
